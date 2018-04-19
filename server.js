0cefa6801d8ddfa7f5618b83c1c95d006dfb66f3
annaboyatyuk <annaboyatyuk@gmail.com>
2018-04-18 19:44:47 -0700

    server

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+// app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
