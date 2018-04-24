7a9449ce2255c04867393462a44491e77ae09f4a
annaboyatyuk <annaboyatyuk@gmail.com>
2018-04-23 17:05:36 -0700

    one book view

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
+
+// Application Middleware
+app.use(cors({origin: '*'}));
 app.use(express.urlencoded({extended:true}));
 
 // Database Setup
@@ -19,18 +22,12 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-// Application Middleware
-app.use(cors({origin: '*'}));
-
 // API Endpoints
 app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
-
-// app.get('*', (req, res) => res.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 
 app.get('/api/v1/books/:id', (req, res) => {
@@ -43,13 +40,5 @@ app.get('/api/v1/books/:id', (req, res) => {
     .catch(console.error);
 })
 
-
-app.post('/api/v1/books', (req, res) => {
-  client.query(`
-    INSERT INTO books (author, title, isbn, image_url, description) VALUES ('${req.body.author}', '${req.body.title}', '${req.body.isbn}', '${req.body.image_url}', '${req.body.description}');
-  `).then(results => res.send(results.rows)).then(console.log('post'));
-})
-
-
-
-
+app.get('/*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
